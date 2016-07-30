4ed62a9260873527eb1360f0d7e92a4c0eb800cd
Makarand <mparigi99@gmail.com>
2016-07-30 16:39:32 +0000

    added search log functionality

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var request = require("request");
-var key = require("./key.js")
+var key = require("./key.js");
+var mongo = require("mongodb").MongoClient;
+
 var app = express();
 
 
@@ -9,30 +11,55 @@ app.get("/api/imagesearch/:query?", function (req, res) {
     var query = req.params.query;
     var offset = req.query.offset || 0;
     
-    request({
-        url: 'https://api.cognitive.microsoft.com/bing/v5.0/images/search?count=10&q=' + query + '&offset=' + offset,
-        headers: {
-            'Ocp-Apim-Subscription-Key': key
-        }
-    }, function (err, response, body) {
-        if (err) throw err;
+    if (!query) res.end("please enter a search term");
+    
+    mongo.connect("mongodb://mparigi:" + key.mlab + "@ds031835.mlab.com:31835/image-search-mparigi", function (error, db) {
+        if (error) throw error;
         
-        var out = [];
-        var data = JSON.parse(body);
-        data['value'].forEach(function (el) {
-            out.push({
-                "url": el['contentUrl'],
-                "snippet": el['name'],
-                "thumbnail": el['thumbnailUrl'],
-                "context": el['hostPageDisplayUrl']
+        var searches = db.collection("searches");
+        searches.insertOne({
+            "term": query,
+            "when": new Date().toUTCString()
+        }, function (er, data) {
+            if (er) throw er;
+            request({
+                url: 'https://api.cognitive.microsoft.com/bing/v5.0/images/search?count=10&q=' + query + '&offset=' + offset,
+                headers: {
+                    'Ocp-Apim-Subscription-Key': key.bing
+                }
+            }, function (err, response, body) {
+                if (err) throw err;
+                var out = [];
+                var data = JSON.parse(body);
+                data['value'].forEach(function (el) {
+                    out.push({
+                        "url": el['contentUrl'],
+                        "snippet": el['name'],
+                        "thumbnail": el['thumbnailUrl'],
+                        "context": el['hostPageDisplayUrl']
+                    });
+                });
+                res.end(JSON.stringify(out));
             });
         });
+    });
+});
+
+
+app.get("/api/latest/imagesearch", function (req, res) {
+    
+    mongo.connect("mongodb://mparigi:" + key.mlab + "@ds031835.mlab.com:31835/image-search-mparigi", function (err, db) {
+        if (err) throw err;
         
-        
-        res.end(JSON.stringify(out));
+        var searches = db.collection("searches");
+        searches.find({}, {_id: 0}).toArray(function (error, docs){
+            db.close();
+            res.end(JSON.stringify(docs));
+        });
     });
     
-
 });
+
+
 
 app.listen(process.env.PORT || 8080);
