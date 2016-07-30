ee8ea0c2b0185c2af6fbf6865b0db5ed9b3ade44
Makarand <mparigi99@gmail.com>
2016-07-30 00:41:48 +0000

    accepts params

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var express = require("express");
-var moment = require("moment");
-
 var app = express();
 
+app.get("/api/imagesearch/:query?", function (req, res) {
+    var query = req.params.query;
+    var offset = req.query.offset;
+    
+    console.log(query + " " + offset);
+    
+    res.end("end");
+});
 
 app.listen(process.env.PORT || 8080);
