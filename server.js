69af1cf80c538c085698fe95e30c6981a3b1f521
Makarand <mparigi99@gmail.com>
2016-07-30 03:57:18 +0000

    added searching feature

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 var express = require("express");
+var request = require("request");
+var key = require("./key.js")
 var app = express();
+
+
 
 app.get("/api/imagesearch/:query?", function (req, res) {
     var query = req.params.query;
-    var offset = req.query.offset;
+    var offset = req.query.offset || 0;
     
-    console.log(query + " " + offset);
+    request({
+        url: 'https://api.cognitive.microsoft.com/bing/v5.0/images/search?count=10&q=' + query + '&offset=' + offset,
+        headers: {
+            'Ocp-Apim-Subscription-Key': key
+        }
+    }, function (err, response, body) {
+        if (err) throw err;
+        
+        var out = [];
+        var data = JSON.parse(body);
+        data['value'].forEach(function (el) {
+            out.push({
+                "url": el['contentUrl'],
+                "snippet": el['name'],
+                "thumbnail": el['thumbnailUrl'],
+                "context": el['hostPageDisplayUrl']
+            });
+        });
+        
+        
+        res.end(JSON.stringify(out));
+    });
     
-    res.end("end");
+
 });
 
 app.listen(process.env.PORT || 8080);
